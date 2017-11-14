89524508a40fa43a7ed4498015d604bfe588d198
cn <nieberga@hm.edu>
2017-11-14 13:59:35 +0100

    Konfig2

--- src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
@@ -1,14 +1,17 @@
 package edu.hm.cs.cnj.cnjbackend.info;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InfoController {
+	
+	@Autowired InfoProperties infoProperties;
 	@RequestMapping("/info")
 	public Info greeting() {
 		Info result = new Info();
-		result.setMessage("OK");
+		result.setMessage(infoProperties.getMessage());
 		return result;
 	}
 }
